f1ee7f6538f4dccf889405f1fdf73cd7bb1e076b
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-06-13 05:12:37 +0300

    Update markdown.py

--- AsAs/plugins/markdown.py
+++ AsAs/plugins/markdown.py
@@ -22,8 +22,8 @@ from telethon.tl.types import (
 )
 from telethon.utils import add_surrogate, del_surrogate
 
-from zthon import zedub
-from zthon.core.logger import logging
+from AsAs import zedub
+from AsAs.core.logger import logging
 
 LOGS = logging.getLogger(__name__)
 
