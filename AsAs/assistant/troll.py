7231bba1e0f2a73957ba96e9902613cb53931f24
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-06-16 18:07:34 +0300

    Update troll.py

--- AsAs/assistant/troll.py
+++ AsAs/assistant/troll.py
@@ -4,14 +4,14 @@ import re
 
 from telethon.events import CallbackQuery
 
-from zthon import zedub
+from AsAs import zedub
 
 
 @zedub.tgbot.on(CallbackQuery(data=re.compile(b"troll_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
-    if os.path.exists("./zthon/troll.txt"):
-        jsondata = json.load(open("./zthon/troll.txt"))
+    if os.path.exists("./AsAs/troll.txt"):
+        jsondata = json.load(open("./AsAs/troll.txt"))
         try:
             message = jsondata[f"{timestamp}"]
             userid = message["userid"]
