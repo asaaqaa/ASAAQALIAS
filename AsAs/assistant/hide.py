e1b03bb0ba68270573c9b7d0aa92721755910f15
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-06-16 08:14:31 +0300

    Update hide.py

--- AsAs/assistant/hide.py
+++ AsAs/assistant/hide.py
@@ -4,18 +4,18 @@ import re
 
 from telethon.events import CallbackQuery
 
-from zthon import zedub
+from AsAs import zedub
 
 
 @zedub.tgbot.on(CallbackQuery(data=re.compile(b"hide_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
-    if os.path.exists("./zthon/hide.txt"):
-        jsondata = json.load(open("./zthon/hide.txt"))
+    if os.path.exists("./AsAs/hide.txt"):
+        jsondata = json.load(open("./AsAs/hide.txt"))
         try:
             reply_pop_up_alert = jsondata[f"{timestamp}"]["text"]
         except KeyError:
-            reply_pop_up_alert = "- عـذراً .. هذه الرسـالة لم تعد موجـوده في سيـرفرات زدثــون"
+            reply_pop_up_alert = "- عـذراً .. هذه الرسـالة لم تعد موجـوده في سيـرفرات الإمبراطور "
     else:
-        reply_pop_up_alert = "- عـذراً .. هذه الرسـالة لم تعد موجـوده في سيـرفرات زدثــون"
+        reply_pop_up_alert = "- عـذراً .. هذه الرسـالة لم تعد موجـوده في سيـرفرات الإمبراطور "
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
