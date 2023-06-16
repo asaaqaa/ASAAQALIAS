364260d2ba90e3fb626b45cd2a4446561859cb9f
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-06-16 18:01:06 +0300

    Update iytdl.py

--- AsAs/assistant/iytdl.py
+++ AsAs/assistant/iytdl.py
@@ -18,7 +18,7 @@ from telethon.events import CallbackQuery
 from telethon.utils import get_attributes
 from wget import download
 
-from zthon import zedub
+from AsAs import zedub
 
 from ..Config import Config
 from ..core import check_owner, pool
@@ -40,7 +40,7 @@ BASE_YT_URL = "https://www.youtube.com/watch?v="
 YOUTUBE_REGEX = re.compile(
     r"(?:youtube\.com|youtu\.be)/(?:[\w-]+\?v=|embed/|v/|shorts/)?([\w-]{11})"
 )
-PATH = "./zthon/cache/ytsearch.json"
+PATH = "./AsAs/cache/ytsearch.json"
 plugin_category = "البوت"
 
 
