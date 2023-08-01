a965e9158a871d914322aa8bd1ecef5d2ed94e4e
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-08-01 06:23:37 +0300

    Update helpers.py

--- AsAs/core/helpers.py
+++ AsAs/core/helpers.py
@@ -6,7 +6,7 @@ from telethon.utils import get_display_name
 
 from .events import NewMessage
 
-LOGGER = logging.getLogger("Reed")
+LOGGER = logging.getLogger("AsAs")
 
 
 def printUser(entity: types.User) -> None:
